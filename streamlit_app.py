2b73335a1ab51d673fe3d8137f8f6ee8099d82fb
nkum2020 <74905732+nkum2020@users.noreply.github.com>
2023-07-03 15:14:49 +0530

    Update streamlit_app.py
    updated double quotes in subheader and caption

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents New age Diner')
 streamlit.header('Breakfast Menu')
-steramlit.subheader('Omega3 and blueberry oatmeals')
-streamlit.caption('kale spinach and Rocket smoothy')
-streamlit.cation('Hard bolied free range eggs')
+steramlit.subheader("Omega3 and blueberry oatmeals")
+streamlit.caption("kale spinach and Rocket smoothy"
+streamlit.cation("Hard bolied free range eggs")
 
