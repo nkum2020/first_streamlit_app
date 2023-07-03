1b43811a987300e28bf82dabd4d4bd46fde90041
nkum2020 <74905732+nkum2020@users.noreply.github.com>
2023-07-03 15:32:15 +0530

    Update streamlit_app.py
    added --streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
-streamlit.title('My parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega3 & blueberry oatmeals')
 streamlit.text("🥗 kale spinach and Rocket smoothy")
 streamlit.text("🐔 Hard bolied free range eggs")
 streamlit.text('🥑🍞 avocado tosts')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
