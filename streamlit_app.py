4bdb821657e5bf4b816b690a1ea4a64292dab74c
nkum2020 <74905732+nkum2020@users.noreply.github.com>
2023-07-03 15:23:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents New age Diner')
 streamlit.header('Breakfast Menu')
-streamlit.subheader('Omega3 and blueberry oatmeals')
+streamlit.text('Omega3 and blueberry oatmeals')
 streamlit.caption("kale spinach and Rocket smoothy")
 streamlit.caption("Hard bolied free range eggs")
 
